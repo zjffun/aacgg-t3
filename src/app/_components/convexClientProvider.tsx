3934ad08cd7979a4dfa45b5df193e8d5b290731e
Jufeng Zhang <zjffun@gmail.com>
2025-09-13 19:42:46 +0800

    fix: type

--- src/app/_components/convexClientProvider.tsx
+++ src/app/_components/convexClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
